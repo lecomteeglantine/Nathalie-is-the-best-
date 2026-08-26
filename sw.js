049286c5628b2034f9c5d15b97682990e9b6782f
lecomteeglantine <lecomte.eglantine@gmail.com>
2026-08-26 10:27:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Nathalie EN · Service Worker V10 */
-const CACHE_VERSION = 'nathalie-v10-2026-08-26';
+/* Nathalie EN · Service Worker V11 */
+const CACHE_VERSION = 'nathalie-v11-2026-08-26';
 const CORE = [
   './',
   './index.html',
